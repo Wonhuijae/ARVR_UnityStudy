07f53e60dbfe0f5ef505b5c72949a617cb4b5b80
Wonhuijae <95402939+Wonhuijae@users.noreply.github.com>
2024-03-20 15:54:02 +0900

    탄환 및 탄환 스크립트 추가

---
+++ Assets/Bullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+        //스크립트와 같은 소속의 게임오브젝트의 transform 컴포넌트
+
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) //on~: 수동 호출x, 상황이 맞으면 자동으로 호출 
+    {
+        //Colider 타입: 충돌된 다른 객체의 정보
+        if (other.tag == "Player")
+        {
+            //스크립트 파일도 컴포넌트이다. 컴포넌트를 가져와서 pc라는 변수에 저장
+            PlayerController pc = other.GetComponent<PlayerController>();
+            //pc가 유효한 변수인지 확인. valid cheak, 유효성 체크
+            if (pc != null) 
+            {
+                pc.Die();
+            }
+        }
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour //: (부모 클래스 이름) -> 상속
 {
     public Rigidbody playerRigidbody;
-    public float speed = 8f; //float 타입 강조, C#에서는 해주는 것이 표준
+    public float speed = 8f; //float 타입 강조, C#에서는 해주는 것이 표준   
 
     // Start is called before the first frame update
     void Start() //처음 한 번 실행
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() //매 프레임마다 실행
     {
+        //Axis: 축, 수평/수직축 입력 감지
+        float xInput = Input.GetAxis("Horizontal"); //수평(좌우 방향키) 입력
+        float zInput = Input.GetAxis("Vertical"); //수직(상하 방향키) 입력
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        //방향을 표시하는 클래스
+        Vector3 newVel = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVel;
+        //velocity: 리지드바디의 멤버 변수(프로퍼티), 주소로 값을 전달받음, 직접 넣어도 상관없음.
+
+        /* 조작감 개선(이전 코드)
         //AddForce: 힘 주기
         //Input: 입력 감지
         //GetKey: 키보드 입력 감지, 누르고 있으면 계속 감지, *GetKeyDown: 누른 순간만 감지
@@ -41,6 +54,12 @@ public class PlayerController : MonoBehaviour //: (부모 클래스 이름) -> 상속
         if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
             playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        }*/
+    }
+
+    public void Die()
+    {
+        //gameObject: 스크립트가 소속된 오브젝트, 따로 선언 필요 없음
+        gameObject.SetActive(false);
     }
 }
