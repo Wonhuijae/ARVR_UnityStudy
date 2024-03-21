18ae1a96591d8a17ff215f2bafe2d0d976249ba7
Wonhuijae <95402939+Wonhuijae@users.noreply.github.com>
2024-03-21 17:18:04 +0900

    탄환 생성기 배치

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Bullet : MonoBehaviour //클래스 이름->대문자
 {
     public float speed = 8f;
     Rigidbody rb;
@@ -13,6 +13,9 @@ public class Bullet : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * speed;
         //스크립트와 같은 소속의 게임오브젝트의 transform 컴포넌트
+        //forward: 앞으로 이동
+        //velocity: 자기중심 좌표
+        //return rotation * Vector3.forward;
 
         Destroy(gameObject, 3f);
     }
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float rateMin = 0.5f;
+    public float rateMax = 3f;
+
+    private Transform target;
+    private float spawnRate;
+    private float timeAfterSpawn;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(rateMin, rateMax);
+        target = FindObjectOfType<PlayerController>().transform;
+        //<>타입의 컴포넌트를 가진 게임오브젝트를 찾는 메서드, 플레이어 위치 추적
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+        //최종적으로 총알을 만든 이후 지난 시간, 흐른 시간이 누적되어 기록됨
+
+        if(timeAfterSpawn > spawnRate)
+        {
+            timeAfterSpawn = 0f; //스폰 누적시간 리셋
+            //탄환 오브젝트 생성, 탄환 생성기의 위치와 회전정보로 생성
+            GameObject bullet = Instantiate(prefab, transform.position, transform.rotation);
+            //생성된 탄환 오브젝트를 target방향으로 회전, 회전값을 수정하는 메서드
+            bullet.transform.LookAt(target); // 한 번만 LookAt()
+            //생성 주기 재할당
+            spawnRate = Random.Range(rateMin, rateMax);
+        }
+    }
+}
